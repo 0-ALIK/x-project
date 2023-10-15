c1376cce202b1a315e8abbdceb60bb5ce248e0e9
crestt15 <showtunez@hotmail.com>
2023-10-14 22:31:29 -0500

    metecion de datos en la tabla

--- src/app/modules/inventario/components/table-inventario/table-inventario.component.ts
+++ src/app/modules/inventario/components/table-inventario/table-inventario.component.ts
@@ -7,6 +7,7 @@ import { PaginatorModule } from 'primeng/paginator';
   templateUrl: './table-inventario.component.html',
   styleUrls: ['./table-inventario.component.css']
 })
+// datos quemados en la tabla de inventario
 export class TableInventarioComponent implements OnInit {
 public products = [
   {code : '001', name: 'Coca Cola',  category: 'Bebidas', type: 'Refresco', quantity: 10, price: 15.00, undefined: 'undefined'},
@@ -18,7 +19,7 @@ public products = [
   {code : '007', name: 'Mango',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
   {code : '008', name: 'Papaya',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
   {code : '009', name: 'Coco',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '010', name: 'Fresa',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
+  {code : '010', name: 'Fresa',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined:'undefined'},
 ];
 
 first = 0;
