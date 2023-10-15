0abb9571b2f6246684b849ade8afdcacd9b89df5
crestt15 <showtunez@hotmail.com>
2023-10-15 15:56:01 -0500

    dsadasd

--- src/app/modules/inventario/components/table-inventario/table-inventario.component.ts
+++ src/app/modules/inventario/components/table-inventario/table-inventario.component.ts
@@ -8,17 +8,16 @@ import { Component, OnInit } from '@angular/core';
 // datos quemados en la tabla de inventario
 export class TableInventarioComponent implements OnInit {
 public products = [
-  {code : '001', name: 'Coca Cola',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
-  {code : '002', name: 'Pepsi',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
-  {code : '003', name: 'Fanta',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
-  {code : '004', name: 'Sprite',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
-  {code : '005', name: 'Manzana',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
-  {code : '006', name: 'Naranja',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 10.00, undefined: 'undefined'},
-  {code : '007', name: 'Limon',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 10.00, undefined: 'undefined'},
-  {code : '008', name: 'Jamaica',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 7.00, undefined: 'undefined'},
-  {code : '009', name: 'Mango',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 5.00, undefined: 'undefined'},
-  {code : '010', name: 'Tamarindo',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 55.00, undefined: 'undefined'}
-
+  {code : '001', name: 'Coca Cola',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 15, undefined: 'undefined'},
+  {code : '002', name: 'Pepsi',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 0, undefined: 'undefined'},
+  {code : '003', name: 'Fanta',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 5, undefined: 'undefined'},
+  {code : '004', name: 'Sprite',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 10, undefined: 'undefined'},
+  {code : '005', name: 'Coca Cola',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 15, undefined: 'undefined'},
+  {code : '006', name: 'Pepsi',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 0, undefined: 'undefined'},
+  {code : '007', name: 'Fanta',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 5, undefined: 'undefined'},
+  {code : '008', name: 'Sprite',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 10, undefined: 'undefined'},
+  {code : '009', name: 'Coca Cola',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 15, undefined: 'undefined'},
+  {code : '010', name: 'Pepsi',  category: 'Bebidas', type: 'Refresco', price: 10.00, quantity: 0, undefined: 'undefined'},
 ];
 first = 0;
 rows = 10;
@@ -34,17 +33,6 @@ onPageChange(event: any){
   this.first = event.first;
   this.rows = event.rows;
 
-}
-
-// funcion para cambiar el color de la cantidad de productos en stock
-getStockClass(quantity: number): string{
-  if(quantity === 0){
-    return 'outofstock';
-  }else if(quantity > 0 && quantity <= 10){
-    return 'lowstock';
-  }else{
-    return 'instock';
-  }
 }
 }
 
