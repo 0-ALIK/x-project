d0540175e433f724401b307082712b96a929a6fc
crestt15 <showtunez@hotmail.com>
2023-10-15 14:55:07 -0500

    cambios en los colores para los stock

--- src/app/modules/inventario/components/table-inventario/table-inventario.component.ts
+++ src/app/modules/inventario/components/table-inventario/table-inventario.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PaginatorModule } from 'primeng/paginator'; 
-
 
 @Component({
   selector: 'app-table-inventario',
@@ -10,18 +8,18 @@ import { PaginatorModule } from 'primeng/paginator';
 // datos quemados en la tabla de inventario
 export class TableInventarioComponent implements OnInit {
 public products = [
-  {code : '001', name: 'Coca Cola',  category: 'Bebidas', type: 'Refresco', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '002', name: 'Pepsi',  category: 'Bebidas', type: 'Refresco', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '003', name: 'Manzana',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '004', name: 'Pera',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '005', name: 'Sandia',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '006', name: 'Melon',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '007', name: 'Mango',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '008', name: 'Papaya',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '009', name: 'Coco',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined: 'undefined'},
-  {code : '010', name: 'Fresa',  category: 'Frutas', type: 'Fruta', quantity: 10, price: 15.00, undefined:'undefined'},
-];
+  {code : '001', name: 'Coca Cola',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '002', name: 'Pepsi',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '003', name: 'Fanta',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '004', name: 'Sprite',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '005', name: 'Manzana',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '006', name: 'Naranja',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '007', name: 'Limon',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '008', name: 'Jamaica',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '009', name: 'Mango',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'},
+  {code : '010', name: 'Tamarindo',  category: 'Bebidas', type: 'Refresco', price: 10, quantity: 15.00, undefined: 'undefined'}
 
+];
 first = 0;
 rows = 10;
 totalRecords = 120;
@@ -36,6 +34,17 @@ onPageChange(event: any){
   this.first = event.first;
   this.rows = event.rows;
 
+}
+
+// funcion para cambiar el color de la cantidad de productos en stock
+getStockClass(stock: number){
+  if(stock > 10){
+    return 'stock-high';
+  }else if(stock <= 10 && stock > 5){
+    return 'stock-medium';
+  }else{
+    return 'stock-low';
+  }
 }
 }
 
--- src/app/modules/inventario/inventario.module.ts
+++ src/app/modules/inventario/inventario.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { InventarioRoutingModule } from './inventario-routing.module';
 import { VerInventarioComponent } from './pages/ver-inventario/ver-inventario.component';
 import { AgregarEditarProductoComponent } from './pages/agregar-editar-producto/agregar-editar-producto.component';
