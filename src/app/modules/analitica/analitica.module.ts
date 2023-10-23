539693fea3ed93596bf2c8acde50818d7750f1bc
EstefanieM21 <estefanie.alzamora@utp.ac.pa>
2023-10-23 16:05:44 -0500

    Creación del html y css

--- src/app/modules/analitica/analitica.module.ts
+++ src/app/modules/analitica/analitica.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AnaliticaRoutingModule } from './analitica-routing.module';
 import { VerDashboardComponent } from './pages/ver-dashboard/ver-dashboard.component';
+
+
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MultiSelectModule } from 'primeng/multiselect';
 import { GenerarReporteClientesComponent } from './components/generar-reporte-clientes/generar-reporte-clientes.component';
 
-/* Acá haces los imports de los componentes de prime ng */
+
 
 @NgModule({
     declarations: [
@@ -15,6 +23,12 @@ import { GenerarReporteClientesComponent } from './components/generar-reporte-cl
 
     imports: [
         CommonModule,
+        FormsModule,
+        DropdownModule,
+        CheckboxModule,
+        InputTextModule,
+        ButtonModule,
+        MultiSelectModule,
         AnaliticaRoutingModule
     ],
     
--- src/app/modules/analitica/components/generar-reporte-clientes/generar-reporte-clientes.component.ts
+++ src/app/modules/analitica/components/generar-reporte-clientes/generar-reporte-clientes.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+interface Formato{
+  nameFormato: string,
+  codeFormato: string
+}
+
+interface TiposDeReportes{
+  nameTipo:string,
+  codeTipo:string 
+}
+
+interface Segmentacion{
+  nameSegmento: string,
+  codeSegmento: string
+}
+
+
 
 @Component({
   selector: 'estefanie-generar-reporte-clientes',
   templateUrl: './generar-reporte-clientes.component.html',
   styleUrls: ['./generar-reporte-clientes.component.css']
 })
-export class GenerarReporteClientesComponent {
+export class GenerarReporteClientesComponent implements OnInit {
+  public segmentos: Segmentacion[] | undefined;
+  public segmentos1: Segmentacion[] | undefined;
+  public selectedSegmentos: Segmentacion[] | undefined;
+  public formatos: Formato[] | undefined;
+  public tiposReportes: TiposDeReportes [] | undefined;
+  public selectedTipos: TiposDeReportes [] | undefined;
+  public loading: boolean= false;
+  
+  public ngOnInit(): void {
+    this.formatos = [
+      {nameFormato: '.txt', codeFormato: 'txt'},
+      {nameFormato: '.cvs', codeFormato: 'cvs'},
+      {nameFormato: '.pdf', codeFormato: 'pdf'}
+    ]
+
+    this.tiposReportes = [
+      {nameTipo: 'Inventario', codeTipo: 'I'},
+      {nameTipo: 'Compras', codeTipo: 'C'},
+      {nameTipo: 'Otros', codeTipo: 'O'}
+    ]
+
+    this.segmentos = [
+      {nameSegmento: 'Bocas del toro', codeSegmento: '1'},
+      {nameSegmento: 'Coclé', codeSegmento: '2'},
+      {nameSegmento: 'Colón', codeSegmento: '3'},
+      {nameSegmento: 'Chiriquí', codeSegmento: '4'},
+      {nameSegmento: 'Darién', codeSegmento: '5'},
+      {nameSegmento: 'Herrera', codeSegmento: '6'},
+      {nameSegmento: 'Los Santos', codeSegmento: '7'},
+      {nameSegmento: 'Panamá', codeSegmento: '8'},
+      {nameSegmento: 'Veraguas', codeSegmento: '9'},
+      {nameSegmento: 'Panamá Oeste', codeSegmento: '10'}
+    
+    ]
+
+    this.segmentos1 = [
+      {nameSegmento: 'Coca Cola', codeSegmento: 'S'},
+      {nameSegmento: 'Pepsi', codeSegmento: 'T'},
+      {nameSegmento: 'Fanta', codeSegmento: 'C'},
+      {nameSegmento: 'Etc', codeSegmento: 'M'}
+
+
+    ]
+  }
+
+    public load(): void {
+      this.loading = true;
+
+      setTimeout(() => {
+        this.loading = false
+      }, 2000);
+    }
 
 }
