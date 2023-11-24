76b676ff66bf8473275bef9662dc0d3e31daffbf
Ana Victoria <113152839+AnghelKupo@users.noreply.github.com>
2023-11-23 22:03:11 -0500

    ver detalles tickets mejorado

--- src/app/modules/tickets/tickets.module.ts
+++ src/app/modules/tickets/tickets.module.ts
@@ -6,15 +6,20 @@ import { VerTicketsComponent } from './pages/ver-tickets/ver-tickets.component';
 
 import { TagModule } from 'primeng/tag';
 import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
 import { VerDetalleComponent } from './pages/ver-detalle/ver-detalle.component';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { TicketsTableComponent } from './components/tickets-table/tickets-table.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ImageModule } from 'primeng/image';
+
 @NgModule({
 	declarations: [
 		VerTicketsComponent,
-  VerDetalleComponent,
-  TicketsTableComponent,
+        VerDetalleComponent,
+        TicketsTableComponent,
+
 	],
 	imports: [
 		CommonModule,
@@ -26,7 +31,11 @@ import { TicketsTableComponent } from './components/tickets-table/tickets-table.
 		ButtonModule,
 		TagModule,
 		SplitButtonModule,
-        InputTextModule
+        DropdownModule,
+        InputTextareaModule,
+        FileUploadModule,
+        ImageModule
+
 	],
 })
 export class TicketsModule { }
